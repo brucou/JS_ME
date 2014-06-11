bd3450a722e1f4fa00220ddf6c05081367f5ff33
brucou <bruno.couriol@gmail.com>
2014-06-11 03:35:39 +0200

    --

--- js/stub.js
+++ js/stub.js
@@ -1,11 +1,18 @@
 /**
  * Created by bcouriol on 7/06/14.
  */
-function stub() {
+function stub(url_param, url_log) {
    logEntry("stub");
-   var my_url = $("#url_param option:selected").val();
-   $("#url").text(my_url);
-   make_article_readable (my_url);
+   var my_url = $("#" + url_param + " option:selected").val();
+   $("#" + url_log).text(my_url);
+   make_article_readable(my_url, activate_read_words_over);
    logWrite(DBG.TAG.DEBUG, "url", my_url);
    logExit("stub");
 }
+
+$(function () {
+   $("#url_param").change(function () {
+      stub("url_param", "url");
+   });
+});
+
